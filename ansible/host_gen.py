c9f059a270da4131de773979490fae5dd2438b98
A-Tsai <chungtsai@gmail.com>
2019-01-08 12:04:00 +0800

    shorten domain name of DataProc

--- ansible/host_gen.py
+++ ansible/host_gen.py
@@ -5,9 +5,16 @@
 from jinja2 import Template
 import socket
 
+# extract domain name of slaves
+# original domain : my-dos-w-0.us-west1-b.c.seqslab-cloud.internal
+# short domain : my-dos-w-0
+# fatal: [my-dos-w-0.us-west1-b.c.seqslab-cloud.internal]: UNREACHABLE! => {"changed": false, "msg": "Failed to connect
+# to the host via ssh: Warning: Permanently added 'my-dos-w-0.us-west1-b.c.seqslab-cloud.internal' (ECDSA) to the list
+# of known hosts.\r\nunix_listener: \"/home/chungtsai_su/.ansible/cp/ansible-ssh-my-dos-w-0.us-west1-b.c.seqslab-cloud.
+# internal-22-chungtsai_su.CvLVq8eadZG6N2vz\" too long for Unix domain socket\r\n", "unreachable": true}
 
 with open('/etc/hadoop/conf/nodes_include', 'r') as f:
-    s = [line.strip() for line in f]
+    s = [line.strip().split(".")[0] for line in f]
 # print(s)
 template = Template("[master]\n{{ master }}\n\n[slave]\n{% for host in slaves -%}\n    {{ host}} \n{% endfor %}\n"
                     "[all:vars]\nansible_user=\nansible_ssh_pass=\nansible_become_pass=\n"

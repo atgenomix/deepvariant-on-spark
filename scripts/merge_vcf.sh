d14a296c661329af2b32c75e81aa19a35ef80e61
zhangJH <changjuihuan@gmail.com>
2019-05-20 09:35:59 +0800

    vcf-deduplicated

--- scripts/merge_vcf.sh
+++ scripts/merge_vcf.sh
@@ -5,9 +5,9 @@ finalfile=$1
 postprocess_variants_out=$2
 
 hadoop fs -text ${postprocess_variants_out}/000.vcf.gz | grep ^# >> head.vcf
-hadoop fs -text ${postprocess_variants_out}/*.vcf.gz | grep -v ^# >> merge-pass.vcf
+hadoop fs -text ${postprocess_variants_out}/*.vcf.gz | grep -v ^# | grep -w "PASS" >> merge-pass.vcf
 cat head.vcf merge-pass.vcf > merge-all.vcf
 bgzip merge-all.vcf
-bcftools norm -f PASS -D merge-all.vcf.gz -O z -o ${finalfile}.vcf.gz
+bcftools norm -D merge-all.vcf.gz -O z -o ${finalfile}.vcf.gz
 
 #########################################################################################

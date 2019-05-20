84d332139054d11e841aebe34df6fd7b995a328a
zhangJH <changjuihuan@gmail.com>
2019-05-20 14:44:46 +0800

    fix bcftools-norm-argument

--- scripts/merge_vcf.sh
+++ scripts/merge_vcf.sh
@@ -8,6 +8,6 @@ hadoop fs -text ${postprocess_variants_out}/000.vcf.gz | grep ^# >> head.vcf
 hadoop fs -text ${postprocess_variants_out}/*.vcf.gz | grep -v ^# | grep -w "PASS" >> merge-pass.vcf
 cat head.vcf merge-pass.vcf > merge-all.vcf
 bgzip merge-all.vcf
-bcftools norm -D merge-all.vcf.gz -O z -o ${finalfile}.vcf.gz
+bcftools norm -d any --no-version merge-all.vcf.gz -O z -o ${finalfile}.vcf.gz
 
 #########################################################################################

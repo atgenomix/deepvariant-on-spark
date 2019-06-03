fccc3c1a09c50f2706937e5c7b8cd33c7249700b
zhangJH <changjuihuan@gmail.com>
2019-06-03 14:07:46 +0800

    test-GPU-driver

--- scripts/initialization-on-dataproc.sh
+++ scripts/initialization-on-dataproc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "[info] initialization_actions.sh start"
-wget https://raw.githubusercontent.com/atgenomix/deepvariant-on-spark/master/scripts/initialization_actions.sh
+wget https://github.com/atgenomix/deepvariant-on-spark/tree/huantest/scripts/initialization_actions.sh
 bash ./initialization_actions.sh
 echo "[info] initialization_actions.sh done"
 

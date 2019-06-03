3601150c601f27ed1832cacb724994970434b69d
zhangJH <changjuihuan@gmail.com>
2019-06-03 16:08:18 +0800

    fix-GPU-driver-problem

--- scripts/initialization-on-dataproc.sh
+++ scripts/initialization-on-dataproc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "[info] initialization_actions.sh start"
-gsutil cp gs://dataproc-3b095611-db2b-4155-8021-a922552cb1aa-us/initialization_actions.sh .
+wget https://raw.githubusercontent.com/atgenomix/deepvariant-on-spark/master/scripts/initialization_actions.sh
 bash ./initialization_actions.sh
 echo "[info] initialization_actions.sh done"
 

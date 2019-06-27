63e3d2b229062d41e04aa994db1a469299ea9c68
zhangJH <changjuihuan@gmail.com>
2019-06-27 22:11:42 +0800

    recover-initialization-scripts-site

--- scripts/WGS/initialization-on-dataproc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "[info] initialization_actions.sh start"
-wget https://raw.githubusercontent.com/atgenomix/deepvariant-on-spark/master/scripts/initialization_actions.sh
-bash ./initialization_actions.sh
-echo "[info] initialization_actions.sh done"
-

2062c58af524bb8a7d2fe77f13b04d758aae4d80
zhangJH <changjuihuan@gmail.com>
2019-07-02 22:46:51 +0800

    sort-function

--- scripts/WES/merge_vcf.sh
+++ scripts/WES/merge_vcf.sh
@@ -6,8 +6,8 @@ postprocess_variants_out=$2
 
 hadoop fs -text ${postprocess_variants_out}/000.vcf.gz | grep ^# > merge-all.vcf
 hadoop fs -text ${postprocess_variants_out}/*.vcf.gz | grep -v ^# | grep -w "PASS" >> merge-all.vcf
-bgzip merge-all.vcf
-bcftools norm -d any --no-version merge-all.vcf.gz -O z -o ${finalfile}.vcf.gz
-rm -rf merge-all.vcf.gz
+bcftools sort merge-all.vcf -O z -o sorted.vcf.gz
+bcftools norm -d any --no-version sorted.vcf.gz -O z -o ${finalfile}.vcf.gz
+rm -rf merge-all.vcf sorted.vcf.gz
 
 #########################################################################################
